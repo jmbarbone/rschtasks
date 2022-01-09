7a71b9fb45ca17183dacff895ccbb43a7065eb95
jmbarbone <jmbarbone@gmail.com>
2022-01-09 12:31:30 -0500

    adds expect

--- tests/testthat/test-schtasks.R
+++ tests/testthat/test-schtasks.R
@@ -11,16 +11,26 @@ test_that("schtasks_query()", {
 })
 
 test_that("check days", {
-  ts <- TaskScheduler$new(days = 1, schedule = "monthly")
-  private(ts, "days")
-  private(ts, "check_days")
 
-  ts <- TaskScheduler$new(days = "friday")
-  private(ts, "days")
+  task <- TaskScheduler$new(
+    task_name = "",
+    days = 1,
+    schedule = "monthly",
+    exec = FALSE
+  )
+
+  expect_identical(private(task, "days"), 1)
+  expect_identical(private(task, "schedule"), "MONTHLY")
+  expect_s3_class(task$create(), c("TaskScheduler", "R6"))
+
+  task$result
+  task$system_call
+  task <- TaskScheduler$new(days = "friday")
+  private(task, "days")
 
 
 
-  # TODO upate with private()
+  # TODO update with private()
   # expect_identical(check_days("1", "monthly"), "1")
   # expect_identical(check_days(1, "monthly"), "1")
   # expect_error(check_days(1))
